e6a9e4740008968c36c18f487de20c906ee8c0eb
nueldotdev <walternuel02@gmail.com>
2025-01-03 17:07:15 +0100

    feat: Setup supabase connection and changed up models, also set up api endpoints, create user is working now

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import AbstractUser
@@ -5,6 +6,7 @@ from django.conf import settings # For AUTH_USER_MODEL
 
 # User model
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=25, blank=False, null=False)
     last_name = models.CharField(max_length=25, blank=True, null=True)
     email = models.EmailField(unique=True, blank=False, null=False)
@@ -13,32 +15,17 @@ class User(AbstractUser):
     image = models.ImageField(upload_to='profile_imgs/', blank=True, null=True)
     is_verified = models.BooleanField(default=False)
 
-    # Might use later
-    # is_active = models.BooleanField(default=True)
-    # is_staff = models.BooleanField(default=False)
-    # is_superuser = models.BooleanField(default=False)
-
     is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']  # Username is still required unless you customize it further.
 
-
     # User focus session stats
     focus_streak = models.IntegerField(default=0)
     longest_focus_streak = models.IntegerField(default=0)
-    total_focus_time = models.IntegerField(default=0)
-    total_break_time = models.IntegerField(default=0)
-
-
+    total_time = models.IntegerField(default=0)
     # User preferences
     mins_for_streak = models.IntegerField(default=10)
-    focus_session_length = models.IntegerField(default=25)  # Length of focus session in minutes
-    focus_session_start_time = models.TimeField(null=True, blank=True, default=None)  # Optional
-    break_session_length = models.IntegerField(default=5)  # Length of break in minutes
-    break_session_start_time = models.TimeField(null=True, blank=True, default=None)  # Optional
-    long_break_length = models.IntegerField(default=15)  # Length of long break in minutes
-    long_break_start_time = models.TimeField(null=True, blank=True, default=None)  # Optional
 
     def get_short_name(self):
         return self.first_name
@@ -55,6 +42,7 @@ class User(AbstractUser):
 
 # Board models
 class Board(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     slug = models.SlugField(unique=True, max_length=200, blank=True)
     name = models.CharField(max_length=25, blank=False, null=False)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -82,36 +70,43 @@ class Board(models.Model):
 
 
 class List(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
     title = models.CharField(max_length=20, null=False, blank=False)
-    # total_cards = models.IntegerField(max=10, )
     created_at = models.DateTimeField(auto_now_add=True)
-
-    # User might want to put one ahead or after another, lets make sure it's position is consistent
     position = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
 
     def save(self, *args, **kwargs):
-        check_position = List.objects.filter(board=self.board)
-        if check_position.exists():
-            max_position = check_position.latest('position').position
-            if max_position < self.position:
-                self.position = max_position + 1
+        if self.pk is None:
+            # New list, add it to the end
+            max_position = List.objects.filter(board=self.board).aggregate(models.Max('position'))['position__max']
+            self.position = (max_position or 0) + 1
+        else:
+            # Existing list, update positions if necessary
+            old_position = List.objects.get(pk=self.pk).position
+            if old_position != self.position:
+                if old_position < self.position:
+                    List.objects.filter(board=self.board, position__gt=old_position, position__lte=self.position).update(position=models.F('position') - 1)
+                else:
+                    List.objects.filter(board=self.board, position__lt=old_position, position__gte=self.position).update(position=models.F('position') + 1)
         super().save(*args, **kwargs)
 
     class Meta:
         db_table = 'lists'
+        ordering = ['position']
 
 
 class Card(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=30, blank=False, null=False)
     list = models.ForeignKey(List, on_delete=models.CASCADE)
     description = models.TextField(max_length=200, blank=True, null=True)
     image_url = models.CharField(blank=True, null=True, max_length=1000000)
     uploaded_img = models.ImageField(upload_to='card_images/', null=True, blank=True)
-    priority = models.ChoicesField(choices=[('High', 'High'), ('Medium', 'Medium'), ('Low', 'Low')], default='', blank=True, null=True)
+    priority = models.CharField(max_length=6, choices=[('High', 'High'), ('Medium', 'Medium'), ('Low', 'Low')], default='', blank=True, null=True)
     due_date = models.DateTimeField(auto_now_add=False, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -123,6 +118,7 @@ class Card(models.Model):
 
 
 class Subtask(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     card = models.ForeignKey(Card, on_delete=models.CASCADE)
     title = models.CharField(max_length=30, blank=False, null=False)
     is_completed = models.BooleanField(default=False)
@@ -136,6 +132,7 @@ class Subtask(models.Model):
 
 
 class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     card = models.ForeignKey(Card, on_delete=models.CASCADE)
     text = models.TextField(max_length=200, blank=False, null=False)
@@ -145,15 +142,14 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
 
-
     class Meta:
         db_table = 'comments'
 
 
 class Reaction(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    # reaction will be an emoji, almost as how discord does it
     reaction_type = models.CharField(max_length=10, blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -161,63 +157,47 @@ class Reaction(models.Model):
         return self.reaction_type
 
 
-
 # models for focus session, focus streak
 class FocusBlock(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.TimeField(blank=True, null=True, default=None)
     description = models.TextField(max_length=200, blank=True, null=True)
-
-    # `frequency_by_week` will be stored in days, and should reset every week
     frequency_by_week = models.IntegerField(default=0)
     total_sessions = models.IntegerField(default=0)
-
-    # `total_time` will be stored in mins, can then be converted to hours in the frontend
-    total_time = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        db_table = 'focus_flows'
+        db_table = 'focus_block'
 
 
 class FocusPath(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)  # Name of the flow
-    blocks = models.ManyToManyField(FocusBlock, through="FocusBlockPath")  # Connect to blocks
-
-    total_time = models.PositiveIntegerField(blank=True, null=True)  # Total time in minutes
-    total_time_limit = models.PositiveIntegerField(blank=True, null=True)  # Optional limit in minutes
+    name = models.CharField(max_length=255)
+    blocks = models.ManyToManyField(FocusBlock, through="FocusBlockPath")
+    total_time = models.PositiveIntegerField(blank=True, null=True)
+    total_time_limit = models.PositiveIntegerField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'focus_paths'
 
 
-class FocusFlowPath(models.Model):
+class FocusBlockPath(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     path = models.ForeignKey(FocusPath, on_delete=models.CASCADE)
     block = models.ForeignKey(FocusBlock, on_delete=models.CASCADE)
-    order = models.PositiveIntegerField()  # Order of the blocks in the block
+    order = models.PositiveIntegerField()
 
     class Meta:
         db_table = 'focus_block_paths'
 
 
-
-class FocusStat(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    total_time = models.IntegerField(default=0) # total time spent on a date
-    total_focus_sessions = models.IntegerField(default=0) # total number of focus sessions on a date
-    focus_block = models.ForeignKey(FocusBlock, on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True)
-
-    class Meta:
-        db_table = 'focus_stats'
-
-
 class FocusSession(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     focus_length = models.IntegerField(default=25)
-    break_length = models.IntegerField(default=5)
     start_time = models.TimeField(blank=True, null=True, default=None)
     focused_on = models.CharField(max_length=100, blank=True, null=True)
     block = models.ForeignKey(FocusBlock, on_delete=models.CASCADE, blank=True, null=True)
@@ -225,7 +205,6 @@ class FocusSession(models.Model):
     minutes_focused = models.IntegerField(default=0)
     entry_date = models.DateField(auto_now_add=True)
 
-    # can only count as a streak if user spends at least minutes set by user
     def save(self, *args, **kwargs):
         if self.minutes_focused >= self.user.mins_for_streak:
             self.user.focus_streak += 1
@@ -244,3 +223,14 @@ class FocusSession(models.Model):
     class Meta:
         db_table = 'focus_sessions'
 
+
+class FocusStat(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    total_time = models.IntegerField(default=0)
+    total_focus_sessions = models.IntegerField(default=0)
+    focus_session = models.ForeignKey(FocusSession, on_delete=models.CASCADE, blank=True, null=True)
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'focus_stats'
--- core/serializers.py
+++ core/serializers.py
@@ -1,13 +1,98 @@
 from rest_framework import serializers
-from .models import User, Board
+from django.db import models
+from .models import User, Board, List
+import uuid
+
+# user serializers
 
 class UserSerializer(serializers.ModelSerializer):
+    # Define a password field that is write-only and required
+    password = serializers.CharField(write_only=True, required=True)
+    id = serializers.UUIDField(read_only=True) 
+
     class Meta:
+        # Specify the model to be serialized
         model = User
-        fields = ['id', 'image', 'first_name', 'last_name', 'email', 'date_joined', 'last_login']
+        # Define the fields to be included in the serialization
+        fields = ['id', 'image', 'first_name', 'last_name', 'email', 'password', 'date_joined', 'last_login']
+        # Ensure the password field is write-only
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Extract the password from the validated data
+        password = validated_data.pop('password', None)
+        # Create a new User instance with the remaining validated data
+        user = User(**validated_data)
+        if password:
+            # Set the user's password using the set_password method
+            user.set_password(password)
+        # Save the user instance to the database
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        # Extract the password from the validated data
+        password = validated_data.pop('password', None)
+        # Update the instance's attributes with the remaining validated data
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            # Update the user's password using the set_password method
+            instance.set_password(password)
+        # Save the updated instance to the database
+        instance.save()
+        return instance
 
 
-class CreateUserSerializer(serializers.ModelSerializer):
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+    
     class Meta:
-        model = User
-        fields = ['image', 'first_name', 'last_name', 'email', 'password']
+        fields = ['email', 'password']
+
+
+
+# board serializers
+
+class BoardSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True) 
+    name = serializers.CharField(required=True)
+    description = serializers.CharField(required=False, default='')
+
+    class Meta:
+        model = Board
+        fields = ['id', 'slug', 'name', 'creator', 'description', 'created_at']
+
+    def create(self, validated_data):
+        return Board.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        return instance
+
+
+# List Serializers
+class ListSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True) 
+
+    class Meta:
+        model = List
+        fields = ['id', 'board', 'title', 'position', 'created_at']
+        read_only_fields = ['created_at']
+
+    def create(self, validated_data):
+        board = validated_data['board']
+        max_position = List.objects.filter(board=board).aggregate(models.Max('position'))['position__max']
+        validated_data['position'] = (max_position or 0) + 1
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        old_position = instance.position
+        new_position = validated_data.get('position', old_position)
+        if old_position != new_position:
+            instance.position = new_position
+            instance.save()
+        return instance
