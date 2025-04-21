a17e8b9c0a782b7740cc8d495cac97ed7a9f7677
Nuel Walter <walternuel02@gmail.com>
2025-04-21 21:02:27 +0100

    Update views.py

--- core/views.py
+++ core/views.py
@@ -74,7 +74,6 @@ class UserView(APIView):
 # Waitlist
 class WaitlistView(APIView):
     permission_classes = [AllowAny] 
-    model = Waitlist
     serializers = WaitlistSerializer
     
     def post(self, request):
