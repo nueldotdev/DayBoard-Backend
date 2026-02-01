625c0e72e4e7c7a86c6bd4c3b27e052e72b0d109
nueldotdev <walternuel02@gmail.com>
2026-02-01 21:21:41 +0100

    fix: commented out unimplemented view

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
   path('add-to-waitlist/', views.WaitlistView.as_view(), name='add-to-waitlist'),
   
   # Image endpoints
-  path('get-images/', views.GetImagesView.as_view(), name='get-images')
+  # path('get-images/', views.GetImagesView.as_view(), name='get-images')
 ]
